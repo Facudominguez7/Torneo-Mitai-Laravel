55a3427064e5c8068cbce40ea27330a17df8fd17
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2024-08-01 01:06:42 -0300

    estilo index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <title>Torneo Mitai</title>
     @vite('resources/css/app.css')
     @vite('resources/js/welcome.js')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
 
 <body class="bg-[--color-primary]">
@@ -139,9 +140,82 @@
             </div>
         </div>
     </nav>
-    @yield('contenido')
-    @yield('admin')
-    @yield('listado-equipos')
+    <main>
+        <style>
+            .bannerFondo {
+                height: 400px;
+            }
+        </style>
+        <div>
+            <div class="bannerFondo bg-green-800 bg-left-top bg-auto bg-repeat-x"
+                style="background-image: url(./img/continuartl_4.png)">
+            </div>
+
+            <div class="-mt-64 ">
+                <div class="w-full text-center">
+                    <p class="text-sm tracking-widest text-white">Subtitle</p>
+                    <h1 class="font-bold text-5xl text-white">
+                        Title
+                    </h1>
+                </div>
+
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-4 ">
+                    <div class="p-2 sm:p-10 text-center cursor-pointer">
+                        <div
+                            class="py-16 max-w-sm rounded overflow-hidden shadow-lg bg-oro-500 hover:bg-oro-600 transition duration-500">
+                            <div class="space-y-10">
+                                <i class="fa fa-spa" style="font-size:48px;"></i>
+                                <div class="px-6 py-4">
+                                    <div class="space-y-5">
+                                        <div class="font-bold text-xl mb-2 text-white">Campeones</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 sm:p-10 text-center cursor-pointer text-white">
+                        <div
+                            class="py-16 max-w-sm rounded overflow-hidden shadow-lg bg-gray-500 hover:bg-gray-600 transition duration-500">
+                            <div class="space-y-10">
+                                <i class="fa fa-head-side-mask" style="font-size:48px;"></i>
+                                <div class="px-6 py-4">
+                                    <div class="space-y-5">
+                                        <div class="font-bold text-xl mb-2">Subcampeones</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 sm:p-10 text-center cursor-pointer ml-2">
+                        <div
+                            class="py-16 max-w-sm rounded overflow-hidden shadow-lg hover:bg-white transition duration-500 bg-white ">
+                            <div class="space-y-10">
+                                <i class="fa fa-swimmer" style="font-size:48px;"></i>
+                                <div class="px-6 py-4">
+                                    <div class="space-y-5">
+                                        <div class="font-bold text-xl mb-2">Valla menos Vencida</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="p-2 sm:p-10 text-center cursor-pointer ml-2">
+                        <div
+                            class="py-16 max-w-sm rounded overflow-hidden shadow-lg hover:bg-white transition duration-500 bg-white ">
+                            <div class="space-y-10">
+                                <i class="fa fa-swimmer" style="font-size:48px;"></i>
+                                <div class="px-6 py-4">
+                                    <div class="space-y-5">
+                                        <div class="font-bold text-xl mb-2">Goleadores</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
