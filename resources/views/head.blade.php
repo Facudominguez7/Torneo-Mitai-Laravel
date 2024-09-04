f72dd57f3c890241cc2e30722df99f2bc8db4002
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2024-09-03 21:30:36 -0300

    arreglo estilo logo

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Torneo Mitai</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('public/fotos/Logo_Mitai_SinFondo.png') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('fotos/Logo_Mitai_SinFondo.png') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/welcome.js')
     @vite('resources/js/app.js')
