5a70da5613c5f5defd065413ae55b1fb2ec9015f
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2024-09-10 12:09:51 -0300

    arreglo etiqueta

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -1,9 +1,4 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Torneo Mitai</title>
-    <link rel="shortcut icon" type="image/png" href="{{ asset('fotos/Logo_Mitai_SinFondo.png') }}">
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-H0PZ81M69T"></script>
     <script>
@@ -16,6 +11,11 @@
 
         gtag('config', 'G-H0PZ81M69T');
     </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Torneo Mitai</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('fotos/Logo_Mitai_SinFondo.png') }}">
     @vite('resources/css/app.css')
     @vite('resources/js/welcome.js')
     @vite('resources/js/app.js')
