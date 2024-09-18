7c184a74d993e3ee3dda94cdb9fbcacf12f497fa
Facundo Diaz Dominguez <facudominguez457@gmail.com>
2024-09-18 13:45:37 -0300

    migraciones planillas y jugadores

---
+++ database/migrations/2024_09_17_190756_create_table_jugadores.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('jugadores', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->date('fecha_nacimiento');
+            $table->string('dni')->unique();
+            $table->integer('idEquipo')->unsigned();
+            $table->foreign('idEquipo')->references('id')->on('equipos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('jugadores');
+    }
+};
