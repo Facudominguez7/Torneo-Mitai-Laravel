1f6b957e324e596a35a69972f485dc2edb77b6aa
Facudominguez7 <facudominguez457@gmail.com>
2025-01-05 21:20:39 -0300

    solucion de error de ruta

--- resources/views/Panel/admin.blade.php
+++ resources/views/Panel/admin.blade.php
@@ -339,7 +339,7 @@
                                     </div>
                                     <div class="flex justify-center mt-4">
                                         <a
-                                            href="{{ route('planilla.show', ['partidoId' => $p->id, 'idEdicion' => $EdicionSeleccionada->id, 'tipoPartido' => 'partido', 'idFecha' => $idFecha ]) }}">
+                                            href="{{ route('planilla.show', ['partidoId' => $p->id, 'idEdicion' => $EdicionSeleccionada->id, 'tipoPartido' => 'partido', 'idFecha' => $idFecha ?? 0 ]) }}">
                                             <button
                                                 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                                 Planillas
