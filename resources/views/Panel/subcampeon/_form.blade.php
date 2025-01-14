1e0884c25f2786064c220a51341b67c51b0095ba
Facudominguez7 <facudominguez457@gmail.com>
2025-01-14 15:05:21 -0300

    solucion no se podia agregar

--- resources/views/Panel/subcampeon/_form.blade.php
+++ resources/views/Panel/subcampeon/_form.blade.php
@@ -6,8 +6,8 @@
     <select name="idEquipo" id="idEquipo"
         class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md">
         @foreach ($equipos as $equipo)
-            <option value="{{ $equipo->id }}"
-                {{ old('idEquipo', $subcampeon->idEquipo) == $equipo->id ? 'selected' : '' }}>
+            <option value="{{ $equipo->idEquipo }}"
+                {{ old('idEquipo', $subcampeon->idEquipo) == $equipo->idEquipo ? 'selected' : '' }}>
                 {{ $equipo->nombreEquipo }}
             </option>
         @endforeach
